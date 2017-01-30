e5be1ca257334b5a225c4f4818818ee2f2b39f01
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-01-30 23:32:32 +0530

    modify db_dump_exporter.py to fix pg_dump path

--- db_dump_exporter.py
+++ db_dump_exporter.py
@@ -4,16 +4,11 @@ import subprocess
 
 class DumpExporter:
     def __init__(self):
-        self.pg_dump_path = "/Opt/PostgreSQL/9.6/bin/pg_dump"
+        self.pg_dump_path = "/opt/PostgreSQL/9.6/bin/pg_dump"
         self.user = "postgres"
         self.host = "localhost"
         self.db_name = "corseco_dashboard"
         self.port = 5432
-        self.pgpass_file_path = os.path.join(os.getcwd(),
-                                             "components",
-                                             "conf",
-                                             ".pgpass")
-        os.putenv("PGPASSFILE", self.pgpass_file_path)
 
     def get_dump(self):
         command = "{0} --host={1} --username={2} --dbname={3} --port={4}".format(self.pg_dump_path,
@@ -23,6 +18,15 @@ class DumpExporter:
                                                                                  self.port)
         response = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
         out, err = response.communicate()
-        with open("sqldump", "wb") as file_handle:
+        with open("db_dump", "wb") as file_handle:
             file_handle.write(out)
-            
+
+
+def main():
+    exporter_obj = DumpExporter()
+    exporter_obj.get_dump()
+
+
+if __name__ == "__main__":
+    main()
+
