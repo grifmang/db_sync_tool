d52131f51444e3796cc8e5e589621873fa790b1a
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-01-31 11:38:44 +0530

    modify db_dump_exporter to add logging functionality

--- db_dump_exporter.py
+++ db_dump_exporter.py
@@ -1,6 +1,9 @@
 import os
 import subprocess
 
+from components.logger import Logger
+from components.conf import settings
+
 
 class DumpExporter:
     def __init__(self):
@@ -10,16 +13,32 @@ class DumpExporter:
         self.db_name = "corseco_dashboard"
         self.port = 5432
 
+        self.log_params = {
+            "error": {
+                "file_name": "error.log",
+                "log_dir": settings.LOG_DIR,
+                "stream_handler": None,
+                "file_handler": True
+            }
+        }
+
+        self.error_logger = Logger(**self.log_params["error"])
+
     def get_dump(self):
-        command = "{0} --host={1} --username={2} --dbname={3} --port={4}".format(self.pg_dump_path,
-                                                                                 self.host,
-                                                                                 self.user,
-                                                                                 self.db_name,
-                                                                                 self.port)
-        response = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
-        out, err = response.communicate()
-        with open("db_dump", "wb") as file_handle:
-            file_handle.write(out)
+        try:
+            command = "{0} --host={1} --username={2} --dbname={3} --port={4}".format(self.pg_dump_path,
+                                                                                     self.host,
+                                                                                     self.user,
+                                                                                     self.db_name,
+                                                                                     self.port)
+            response = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE)
+            out, err = response.communicate()
+            with open("db_dump", "wb") as file_handle:
+                file_handle.write(out)
+                
+        except Exception as err:
+            self.error_logger.error("{}::{}".format("DumpExporter.get_dump",
+                                                    err.message))
 
 
 def main():
@@ -29,4 +48,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
