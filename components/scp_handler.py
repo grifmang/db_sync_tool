165f0ba83328b6389690a3b0bb799dc1760db640
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-02-01 14:58:27 +0530

    modify scp_handler.py to add key based authentication in sftp

--- components/scp_handler.py
+++ components/scp_handler.py
@@ -5,9 +5,12 @@ from logger import Logger
 
 
 class SCPHandler:
-    def __init__(self, host, username, password, port=22):
+    def __init__(self, host, username, port=22):
         self.sftp = None
         self.sftp_open = False
+        private_key_file_path = "/home/sud/.ssh/id_rsa"
+        private_key = paramiko.RSAKey.from_private_key_file(private_key_file_path, password="Adioscrapper13130#")
+
 
         # initialize logging parameters
         self.log_params = {
@@ -24,10 +27,10 @@ class SCPHandler:
         try:
             self.transport = paramiko.Transport((host, port))
             self.transport.connect(username=username,
-                                   password=password)
+                                   pkey=private_key)
 
         except Exception as err:
-            self.logger.error("{}::{}".format("SCPHandler.__init__",
+            self.logger.logger.error("{}::{}".format("SCPHandler.__init__",
                                               err.message))
 
     def open_sftp_connection(self):
@@ -54,18 +57,17 @@ class SCPHandler:
             self.open_sftp_connection()
             self.sftp.put(local_path, remote_path)
 
-        except Exception as err:
-            self.logger.error("{}::{}".format("SCPHandler.put_file",
+        except paramiko.SSHException as err:
+            self.logger.logger.error("{}::{}".format("SCPHandler.put_file",
                                               err.message))
 
 
 def main():
-    user = "luvm"
-    host = "192.168.1.33"
+    user = "root"
+    host = "128.199.241.163"
     port = 22
-    password = "vm123#"
-    scp_handler = SCPHandler(host, user, password, port)
-    scp_handler.put_file("/home/sud/alag.py", "/home/luvm/alag.py")
+    scp_handler = SCPHandler(host, user, port)
+    scp_handler.put_file("/home/sud/alag.py", "/root/alag.py")
 
 
 if __name__ == "__main__":
