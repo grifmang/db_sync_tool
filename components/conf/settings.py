20efaa002662c4ea5ece0437c81595a33ca20f28
sudhanshu singh chauhan <minimalcrap@gmail.com>
2017-02-01 14:54:40 +0530

    modify settings.py to modify db config file path

--- components/conf/settings.py
+++ components/conf/settings.py
@@ -1,17 +1,23 @@
 import os
 
 
+# directory path for placing log files
 LOG_DIR = os.path.join(os.path.dirname(__file__), "..", "..", "log")
-CONFIG_FILE_DIR = ""
 
+# file containing db connection parameters & other settings
+CONFIG_FILE_PATH = os.path.join(os.path.dirname(__file__),
+                                "dbconfig")
 
 # REMOTE SERVER CREDENTIALS
-
 REMOTE_SERVER_CREDENTIAL = {
     "my_lubuntu_vm": {
         "host": "192.168.1.33",
         "port": 22,
         "username": "luvm",
-        "password": "vm123#"
+    },
+    "remote_vps": {
+        "host": "128.199.241.163",
+        "port": 22,
+        "username": "root",
     }
 }
